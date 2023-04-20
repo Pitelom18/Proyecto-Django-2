5cab484694f43e501f878cbc271f91361f970e9f
Pitelom18 <pitelom18@gmail.com>
2023-04-20 14:34:11 -0500

    se actualiza gitignore

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=5z$20c^_@@jxz=26b@0-j=1s^-i-pm(@%%6+k+p)h2cx^6!i$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
